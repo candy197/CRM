1dcd3f057e9e52251bff2454d6f16049198acb39
candy197 <candy.jacob1997@gmail.com>
2022-11-19 19:15:55 +0530

    update

--- timesheet/urls.py
+++ timesheet/urls.py
@@ -6,7 +6,12 @@ urlpatterns = [
 
      path("",views.index,name='timesheet'),
      path("rootlogin",views.rootlogin,name='timesheet'),
-     path("index",views.index,name='timesheet')
+     path("index",views.index,name='timesheet'),
+     path("LeaveApply",views.LeaveApply,name='timesheet'),
+     path("Timesheet",views.Timesheet,name='timesheet'),
+     path("LeaveReport",views.LeaveReport,name='timesheet'),
+     path("TimesheetReport",views.TimesheetReport,name='timesheet'),
+     path("OverallReport",views.OverallReport,name='timesheet'),
     
    
 ]
--- timesheet/views.py
+++ timesheet/views.py
@@ -5,3 +5,14 @@ def index(request):
     return render(request,'index.html')
 def rootlogin(request):
     return render(request,"rootlogin.html")
+
+def LeaveApply(request):
+    return render(request,"LeaveApply.html")
+def Timesheet(request):
+    return render(request,"Timesheet.html")
+def LeaveReport(request):
+    return render(request,"LeaveReport.html")
+def TimesheetReport(request):
+    return render(request,"TimesheetReport.html")
+def OverallReport(request):
+    return render(request,"OverallReport.html")
