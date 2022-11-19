c30788c266d3f51c1328557a7353b181b457729e
Candy Jacob <42080597+candy197@users.noreply.github.com>
2022-11-19 10:34:33 +0530

    update

--- timesheet/views.py
+++ timesheet/views.py
@@ -3,3 +3,5 @@ from django.shortcuts import render,HttpResponse
 # Create your views here.
 def index(request):
     return render(request,'index.html')
+def rootlogin(request):
+    return render(request,"rootlogin.html")
