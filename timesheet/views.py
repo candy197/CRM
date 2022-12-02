21aca5f1ad55aa0e0ba52b2a9f7db8437e6ebb4c
Candy Jacob <42080597+candy197@users.noreply.github.com>
2022-12-02 22:09:50 +0530

    commit

--- timesheet/views.py
+++ timesheet/views.py
@@ -1,7 +1,19 @@
-from django.shortcuts import render,HttpResponse
-
+from django.shortcuts import render,HttpResponse,redirect
+from django.contrib.auth.models import User
+from django.contrib import messages
+from django.contrib.auth import authenticate,login
 # Create your views here.
 def index(request):
+    if request.method == "POST":
+        username = request.POST['user']
+        password = request.POST['pass']
+        user = authenticate(username=username,password=password)
+        if user is not None:
+            login(request,user)
+            return render(request,"Timesheet.html")
+        else:
+            messages.error(request,"Bad Creds!! ")
+            return render(request,"index.html")
     return render(request,'index.html')
 def rootlogin(request):
     return render(request,"rootlogin.html")
@@ -16,4 +28,15 @@ def TimesheetReport(request):
 def OverallReport(request):
     return render(request,"OverallReport.html")
 def register(request):
-    return render(request,"register.html")
+    if request.method == "POST":
+        fullname = request.POST['fname'] 
+        username =  request.POST['user']
+        email = request.POST['email']
+        passwd = request.POST['pass']
+        myuser = User.objects.create_user(username,email,passwd)
+        myuser.fullname_name = fullname
+        myuser.save()
+        messages.success(request,"Your account has been created")
+        return render(request,'index.html')
+    else:
+        return render(request,"register.html")
